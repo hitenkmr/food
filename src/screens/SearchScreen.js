eb91ebcb6d1733c88818ccb8dd4ce0dc8efe5049
Hitender Kumar <hitender@backbase.com>
2021-10-18 20:55:41 +0530

    create 'SearchBar' component to seperate search related elements

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import { ScrollView, Image, TextInput, View, Text, StyleSheet } from 'react-native';
-import { Feather } from "@expo/vector-icons";
 
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 import ItemsSection from '../components/ItemsSection';
 
 const SearchScreen = () => {
 
-  const [term, setTerm] = useState('')
-  console.log(term)
+  const [term, setTerm] = useState('');
 
   var commonItem = { title: 'Fish city', starsCount: 20, reviewsCount: 30 };
 
@@ -27,30 +26,16 @@ const SearchScreen = () => {
   var filteredSections = sections;
 
   return (
-    <ScrollView style={styles.container}>
-
-      <View style={styles.searchContainer}>
-        <Feather name='search' style={styles.searchIcon} />
-
-        <TextInput
-          style={styles.textInput}
-          placeholder="Search"
-          placeholderTextColor='rgb(182,180,181)'
-          value={term}
-          onChangeText={(searchText) => {
-            setTerm(searchText)
-          }}
-          onEndEditing={() => { console.log('submitted') }}
-        />
-      </View>
-
-      <View style={{ alignContent: 'space-between' }}>
-        <ItemsSection items={filteredSections[0].items} style={styles.section} headerName={filteredSections[0].name}></ItemsSection>
-        <ItemsSection items={filteredSections[1].items} style={styles.section} headerName={filteredSections[1].name}></ItemsSection>
-        <ItemsSection items={filteredSections[2].items} style={styles.section} headerName={filteredSections[2].name}></ItemsSection>
-      </View>
-
-    </ScrollView>
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
+      />
+      <ItemsSection items={filteredSections[0].items} style={styles.section} headerName={filteredSections[0].name}></ItemsSection>
+      <ItemsSection items={filteredSections[1].items} style={styles.section} headerName={filteredSections[1].name}></ItemsSection>
+      <ItemsSection items={filteredSections[2].items} style={styles.section} headerName={filteredSections[2].name}></ItemsSection>
+    </View>
   );
 };
 
