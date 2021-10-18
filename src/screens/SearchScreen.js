463b53c9cdc0dc92580b6934b16584e764c6cfc8
Hitender Kumar <hitender@backbase.com>
2021-10-18 18:54:21 +0530

    use expo default icon

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ScrollView, Image, TextInput, View, Text, StyleSheet } from 'react-native';
+import { Feather } from "@expo/vector-icons";
+
 import ItemsSection from '../components/ItemsSection';
 
 const SearchScreen = () => {
@@ -18,7 +20,8 @@ const SearchScreen = () => {
     <ScrollView style={styles.container}>
 
       <View style={styles.searchContainer}>
-        <Image style={styles.searchIcon} source={require('../../assets/ic_search.png')} />
+        <Feather name='search' style={styles.searchIcon} size={25}/>
+
         <TextInput
           style={styles.textInput}
           placeholder="Search"
